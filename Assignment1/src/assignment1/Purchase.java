7cb308b2319385d95dcaab65543666bb61d4bb66
c0647711 <c0647711@SB-N208-15.lambton.on.ca>
2015-01-14 16:21:59 -0500

    created variables in order class. test When Neither Customer Id Nor Customer Name Exists

--- Assignment1/src/assignment1/Purchase.java
+++ Assignment1/src/assignment1/Purchase.java
@@ -3,13 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package assignment1;
 
 /**
  *
- * @author c0644881
+ * @author c0644696
  */
 public class Purchase {
-    
+
+    private String productId;
+    private int quantity;
+
+    public Purchase() {
+        this.productId = "";
+        this.quantity = 0;
+    }
+
+    public Purchase(String productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
